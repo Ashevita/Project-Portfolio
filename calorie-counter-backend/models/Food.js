59e295463c8901285a3439f49221a2aa36373392
Ashevita <henri.53@hotmail.fr>
2025-07-15 11:07:04 +0200

    Delete calorie-counter-backend directory

--- calorie-counter-backend/models/Food.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const FoodSchema = new mongoose.Schema({
-    name: String,
-    calories: Number,
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-});
-
-module.exports = mongoose.model("Food", FoodSchema);
--- calorie-counter-backend/routes/foodRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { searchFoods } = require('../controllers/foodController');
-
-const router = express.Router();
-
-// Route de recherche des aliments
-router.get('/search', searchFoods);
-
-module.exports = router;
